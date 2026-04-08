1ab21efe3942215ce201d8d3375b3d31175d7140
yoseyusMainComputer <zaquijobs@gmail.com>
2026-04-08 15:57:32 +0200

    optimize texture compression

--- src/Experience/sources.ts
+++ src/Experience/sources.ts
@@ -65,51 +65,28 @@ export default [
         path: "fancy_picture_frame_01_1k.gltf/fancy_picture_frame_01_1k.gltf"
     },
     {
-        name: 'chairModel',
+        name: 'chair',
         type: 'gltfModel',
         path: "mid_century_lounge_chair_1k.gltf/mid_century_lounge_chair_1k.gltf"
     },
     {
-        name: 'chairTxt_arm',
-        type: 'texture',
-        path: "mid_century_lounge_chair_1k.gltf/textures/mid_century_lounge_chair_arm_1k.jpg"
-    },
-    {
-        name: 'chairTxt_diff',
-        type: 'texture',
-        path: "mid_century_lounge_chair_1k.gltf/textures/mid_century_lounge_chair_diff_1k.jpg"
-    },
-    {
-        name: 'chairTxt_nor',
-        type: 'texture',
-        path: "mid_century_lounge_chair_1k.gltf/textures/mid_century_lounge_chair_nor_gl_1k.jpg"
-    },
-    /* {
-        name: 'environmentMapTexture',
-        type: 'cubeTexture',
-        path:
-        [
-            'textures/environmentMap/px.jpg',
-            'textures/environmentMap/nx.jpg',
-            'textures/environmentMap/py.jpg',
-            'textures/environmentMap/ny.jpg',
-            'textures/environmentMap/pz.jpg',
-            'textures/environmentMap/nz.jpg'
-        ]
-    },
-    {
-        name: 'grassColorTexture',
-        type: 'texture',
-        path: 'textures/dirt/color.jpg'
-    },
-    {
-        name: 'grassNormalTexture',
-        type: 'texture',
-        path: 'textures/dirt/normal.jpg'
-    },
-    {
-        name: 'foxModel',
+        name: 'telephone',
         type: 'gltfModel',
-        path: 'models/Fox/glTF/Fox.gltf'
-    } */
+        path: "vintage_telephone_wall_clock_1k.gltf/vintage_telephone_wall_clock_1k.gltf"
+    },
+    {
+        name: 'picture2',
+        type: 'gltfModel',
+        path: "fancy_picture_frame_02_1k.gltf/fancy_picture_frame_02_1k.gltf"
+    },
+    {
+        name: 'chineseTable',
+        type: 'gltfModel',
+        path: "chinese_tea_table_1k.gltf/chinese_tea_table_1k.gltf"
+    },
+    {
+        name: 'light',
+        type: 'gltfModel',
+        path: "industrial_wall_sconce_1k.gltf/industrial_wall_sconce_1k.gltf"
+    },
 ]
